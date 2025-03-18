c62f4cae4055f2710393d933b282f589dc4200b4
Nick <nmooreco@gmail.com>
2025-03-17 19:47:18 -0600

    Module 1

---
+++ Module1/part1.py
@@ -0,0 +1,6 @@
+num1 = int(input("Enter number1: "))  
+num2 = int(input("Enter number2: "))
+
+print("\nAdding the 2 numbers (", num1, "+", num2, ") =", num1 + num2)
+print("Subtracting num1 from num2 (", num1, "-", num2, ") =", num1 - num2)
+print("Subtracting num2 from num1 (", num2, "-", num1, ") =", num2 - num1)
---
+++ Module1/part2.py
@@ -0,0 +1,5 @@
+num1 = int(input("Enter number1: "))  
+num2 = int(input("Enter number2: "))
+
+print("Multiplying the 2 numbers (", num1, "*", num2, ") =", num1 * num2)
+print("Dividing num1 by num2 (", num1, "/", num2, ") =", num1 / num2)
